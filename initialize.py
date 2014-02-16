f3d9f1cdeee9cd88811d52cebd86a66d23aa25ea
Brooks Willis <christopher.willis@students.olin.edu>
2014-02-16 13:42:13 -0500

    stand command works

--- initialize.py
+++ initialize.py
@@ -16,16 +16,43 @@ a = ankle
 
 def setup():
     dyn = USB2Dynamixel_Device()
-    r = Robotis_Servo(dyn, 1, 'MX')
-    return (dyn, r)
+    rhTwist = Robotis_Servo(dyn, 7, 'MX')
+    lhTwist = Robotis_Servo(dyn, 8, 'MX')
+    rhTilt = Robotis_Servo(dyn, 9, 'MX')
+    lhTilt = Robotis_Servo(dyn, 10, 'MX')
+    rhSwing = Robotis_Servo(dyn, 11, 'MX')
+    lhSwing = Robotis_Servo(dyn, 12, 'MX')
+    rkExt = Robotis_Servo(dyn, 13, 'MX')
+    lkExt = Robotis_Servo(dyn, 14, 'MX')
+    raExt = Robotis_Servo(dyn, 15, 'MX')
+    laExt = Robotis_Servo(dyn, 16, 'MX')
+    raTilt = Robotis_Servo(dyn, 17, 'MX')
+    laTilt = Robotis_Servo(dyn, 18, 'MX')
+
+    
+    return (dyn, rhTwist, lhTwist, rhTilt, lhTilt, rhSwing, lhSwing, rkExt, lkExt, raExt, laExt, raTilt, laTilt)
 
 def main():
-    dyn, r = setup()
-    command = 'start'
-    while command != 10:
-        command = raw_input('Next Command? ')
-        command = float(command)
-        r.move_angle(radians(command))    
+    dyn, rhTwist, lhTwist, rhTilt, lhTilt, rhSwing, lhSwing, rkExt, lkExt, raExt, laExt, raTilt, laTilt  = setup()
+    command = raw_input('Next Command? ')
+    while command != 'end':
+        if command == 'stand':
+            rhTwist.move_angle(0)
+            lhTwist.move_angle(0)
+            rhTilt.move_angle(0)
+            lhTilt.move_angle(0)
+            rhSwing.move_angle(0)
+            lhSwing.move_angle(0)
+            rkExt.move_angle(0)
+            lkExt.move_angle(0)
+            raExt.move_angle(0)
+            laExt.move_angle(0)
+            raTilt.move_angle(0)
+            laTilt.move_angle(0)
+        else:
+            command = float(command)
+            lhSwing.move_angle(radians(command))   
+        command = raw_input('Next Command? ')     
      
 if __name__ == '__main__':
     main()
